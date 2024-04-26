3f8fdcdf7a9ee7677b3917faeb9df5f55ebfa2f6
pawkim <pawkim@github.com>
2024-04-26 23:05:23 +0200

    Praca domowa 1

---
+++ Praca domowa 1/PracaDomowa1/PracaDomowa1/PracaDomowa1.cpp
@@ -0,0 +1,27 @@
+﻿// Paweł Kimel
+// Obliczanie objętości kuli, na podstawie podanego promienia
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    std::locale::global(std::locale("pl_PL.utf8"));
+
+    std::cin.imbue(std::locale());
+    std::wcout.imbue(std::locale());
+
+    constexpr double coeff = M_PI * 4 / 3;
+    double r;
+    std::wcout << L"Podaj długość promienia: ";
+    std::cin >> r;
+
+    const auto result = coeff * r * r * r;
+    std::wcout << L"Objętość kuli o promieniu " << r << L" wynosi " << result << '\n';
+
+    std::wcout << L"\nNaciśnij dowolny klawisz, aby zakończyć...";
+    std::cin.get();
+}
